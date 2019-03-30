f58ff1b765c1fa4ad1c2be64abc714bd03ebd3dc
Seven <seven@Sevens-MacBook-Pro.local>
2019-03-31 03:39:20 +0700

    Implement movie resource

--- models/genre.js
+++ models/genre.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const Genre = mongoose.model('Genre', mongoose.Schema({
+const genreSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
         minlength: 5,
         maxlength: 50,
     }
-}));
+});
+const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
     const schema = {
@@ -17,5 +18,6 @@ function validateGenre(genre) {
     return Joi.validate(genre, schema);
 }
 
+exports.genreSchema = genreSchema;
 exports.Genre = Genre;
 exports.validate = validateGenre;
