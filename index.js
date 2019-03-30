43d6186e981f85bda1ee1a3e743b1d4def7c6ec2
Seven <seven@Sevens-MacBook-Pro.local>
2019-03-31 00:37:12 +0700

    Implement mongoose

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const genres = require('./routes/genres');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(e => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
