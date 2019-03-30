5bb6865b826858aa96af34cdb4f61e1f45b5558c
Seven <seven@Sevens-MacBook-Pro.local>
2019-03-30 13:28:17 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const genres = require('./routes/genres');
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/genres', genres);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+    console.log(`Listening on port ${port}...`)
+});
