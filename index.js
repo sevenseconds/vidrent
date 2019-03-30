f76a7fa0c74019b5ad9ce3938c02b43ba966ef75
Seven <seven@Sevens-MacBook-Pro.local>
2019-03-31 01:14:27 +0700

    Implement customer resource

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 
 const app = express();
 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost/vidly')
 
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 
