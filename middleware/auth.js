f41b93d247bed5eb623584da6fd18c56402b11b9
Seven <seven@Sevens-MacBook-Pro.local>
2019-03-31 15:36:15 +0700

    Authentication middle ware

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+module.exports = function auth(req, res, next) {
+    const token = req.header('x-auth-token');
+    if (!token) {
+        return res.status(401).send('Access denied. No token provided.');
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        res.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).send('Invalid token.');
+    }
+}
--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const { Genre, validate } = require('../models/genre');
 
 const router = express.Router();
@@ -17,7 +18,7 @@ router.get('/:id', async (req, res) => {
     res.send(genre);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) {
         return res.status(400).send(error.message);
@@ -28,7 +29,7 @@ router.post('/', async (req, res) => {
     res.send(genre);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) {
         return res.status(400).send(error.message);
@@ -46,7 +47,7 @@ router.put('/:id', async (req, res) => {
     res.send(genre);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id);
     if (!genre) {
         return res.status(404).send('The genre with the given ID was not found.');
